66ed5355935f55787d6aa3dd0e3fc02182b72db3
AlexsandroFilho <alexsandro.filho0402@gmail.com>
2025-08-17 11:32:10 -0300

    Feita as implementações dos atributos da Classe Smartphone. Classes Nokia e Iphone herdando da Classe Smartphone, e implementando o metodo abstrado InstalarAlicativo, e feito os testes na Classe Main

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine($"Smarthphone Nokia:");
+Nokia nokia = new Nokia("12345678", "Modelo Z", "111111111", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Discord");
+
+Console.WriteLine("\n");
+
+Console.WriteLine($"Smarthphone Iphone:");
+Iphone iphone = new Iphone("2578", "Modelo X", "777777777", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
